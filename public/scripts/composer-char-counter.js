2433e0aeb318cf80c4f68e9b6f66b6f30c14d1fc
Chandra <krsna.c@gmail.com>
2017-11-15 21:16:53 +0000

    refactored char-counter with Airbnb style guide

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,20 @@
 $(function () {
+
   var maxChars = 140;
-  $(".new-tweet textarea").on("input", function(){
+
+  var countChars = function displayCharacterCountInCounter() {
     var $this = $(this);
     var charsUsed = $this.val().length;
     var remainingLetterBudget = maxChars - charsUsed;
-    // var remainingLetterBudget = 140 - this.value.length;
-    // var $counter = $(this).siblings('.counter');
     var $counter = $this.closest('.new-tweet').find('.counter');
     $counter.text(remainingLetterBudget);
-    if(remainingLetterBudget < 0) {
-      $counter.addClass("exceeded");
-    } else {
-      $counter.removeClass("exceeded");
-    };
-  });
+      if(remainingLetterBudget < 0) {
+        $counter.addClass("exceeded");
+      } else {
+        $counter.removeClass("exceeded");
+      };
+  };
+
+  $(".new-tweet textarea").on("input", countChars);
+
 });
