038b65872fc6f26923a0aa6d6c9314a533e511d3
Chandra <krsna.c@gmail.com>
2017-11-15 21:04:40 +0000

    added z-index to nav-bar and improved char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,12 @@
 $(function () {
-  $(".new-tweet textarea").keyup(function(){
-    var remainingLetterBudget = 140 - $(this).val().length;
+  var maxChars = 140;
+  $(".new-tweet textarea").on("input", function(){
+    var $this = $(this);
+    var charsUsed = $this.val().length;
+    var remainingLetterBudget = maxChars - charsUsed;
     // var remainingLetterBudget = 140 - this.value.length;
-    var $counter = $(this).siblings('.counter');
+    // var $counter = $(this).siblings('.counter');
+    var $counter = $this.closest('.new-tweet').find('.counter');
     $counter.text(remainingLetterBudget);
     if(remainingLetterBudget < 0) {
       $counter.addClass("exceeded");
