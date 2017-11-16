715d6fd0498f24eec9f5cd407d640855d4bd7b20
Chandra <krsna.c@gmail.com>
2017-11-16 01:11:36 +0000

    reset counter after a new tweet is tweeted

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,53 +5,6 @@
  */
 $(function () {
 
-  // var data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-  //         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-  //         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-  //       },
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-  //         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-  //         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-  //       },
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense, je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Johann von Goethe",
-  //       "avatars": {
-  //         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-  //         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-  //         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-  //       },
-  //       "handle": "@johann49"
-  //     },
-  //     "content": {
-  //       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-  //     },
-  //     "created_at": 1461113796368
-  //   }
-  // ];
-
   function escape(str) {
     var div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
@@ -98,7 +51,12 @@ $(function () {
   function handleNewTweet(event) {
     event.preventDefault();
     var $form = $(this);
-    // console.log($form.serialize());
+    var tweet = $form.find('textarea').val();
+    // console.log(Array.from(tweet).length);
+    if (Array.from(tweet).length = 0) {
+      alert("You can't tweet that!");
+      return;
+    }
     $.ajax({
       method: 'POST',
       url: '/tweets',
@@ -106,15 +64,13 @@ $(function () {
     })
       .done(function () {
         $form.find('textarea').val("");
+        $form.closest('.new-tweet').find('.counter').text('140');
         loadTweets();
       })
-// should I redirect here? how?
   };
 
-  // $form = $('.new-tweet form');
+  var $form = $('.new-tweet form');
 
-  // $form.on('submit', handleNewTweet);
-
-
+  $form.on('submit', handleNewTweet);
 
 });
